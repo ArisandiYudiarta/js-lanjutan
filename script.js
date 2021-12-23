181ab4f08138e88d539ba137474201093eac9bea
ArisandiYudiarta <arisandiyudiarta21@gmail.com>
2021-12-23 22:40:37 +0800

    object.create

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 // 1. Object Iteration============================================
 
+//PROBLEM : repeating urself
+
 // // Object 1
 // let mahasiswa1 = {
 //     //property
@@ -27,44 +29,69 @@
 // }
 
 //2. Function Declaration=========================================
-// function Mahasiswa(nama, energi){
-//     let mahasiswa = {};
-//     mahasiswa.nama = nama;
-//     mahasiswa.energi = energi;
 
-//     mahasiswa.makan = function(porsi){
-//         this.energi += porsi;
-//         console.log(`Halo ${this.nama}, Selamat makan!`);
-//     }
+//ini yang dihubungkan
+const methodMahasiswa = {
 
-//     mahasiswa.main = function(jam){
-//         this.energi -= jam;
-//         console.log(`Halo ${this.nama}, Selamat bermain!`);
-//     }
+    makan: function(porsi){
+        this.energi += porsi;
+        console.log(`Halo ${this.nama}, Selamat makan!`);
+    },
 
-//     return mahasiswa;
-// }
-// // Object 1
-// let arisandi = Mahasiswa('Arisandi', 100);
-// // Object 2
-// let ambara = Mahasiswa('Arisandi', 100);
+    main:  function(jam){
+        this.energi -= jam;
+        console.log(`Halo ${this.nama}, Selamat bermain!`);
+    },
+
+    tidur: function(jam){
+        this.energi += jam*2;
+        console.log(`Halo ${this.nama}, Silahkan tidur!`);
+    },
+
+};
+
+function Mahasiswa(nama, energi){
+    // menghubungkan dengan object diatas.
+    let mahasiswa = Object.create(methodMahasiswa);
+    mahasiswa.nama = nama;
+    mahasiswa.energi = energi;
+    // mahasiswa.makan = methodMahasiswa.makan;
+    // mahasiswa.main = methodMahasiswa.main;
+    // mahasiswa.tidur = methodMahasiswa.tidur;
+
+    // mahasiswa.makan = function(porsi){
+    //     this.energi += porsi;
+    //     console.log(`Halo ${this.nama}, Selamat makan!`);
+    // }
+
+    // mahasiswa.main = function(jam){
+    //     this.energi -= jam;
+    //     console.log(`Halo ${this.nama}, Selamat bermain!`);
+    // }
+
+    return mahasiswa;
+}
+// Object 1
+let arisandi = Mahasiswa('Arisandi', 100);
+// Object 2
+let ambara = Mahasiswa('Ambara', 100);
 
 //the only thing u only use really.
 //3. Constructor Function=========================================
 //keyword new
-function Mahasiswa(nama, energi){
-    this.nama = nama;
-    this.energi = energi;
+// function Mahasiswa(nama, energi){
+//     this.nama = nama;
+//     this.energi = energi;
 
-    this.makan = function(porsi){
-        this.energi += porsi;
-        console.log(`Halo ${this.nama}, Selamat makan!`);
-    }
+//     this.makan = function(porsi){
+//         this.energi += porsi;
+//         console.log(`Halo ${this.nama}, Selamat makan!`);
+//     }
 
-    this.main = function(jam){
-        this.energi -= jam*10;
-        console.log(`Halo ${this.nama}, Selamat bermain!`);
-    }
-}
+//     this.main = function(jam){
+//         this.energi -= jam*10;
+//         console.log(`Halo ${this.nama}, Selamat bermain!`);
+//     }
+// }
 
-let arisandi = new Mahasiswa('Arisandi', 100);
+// let arisandi = new Mahasiswa('Arisandi', 100);
